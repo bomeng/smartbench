ab5c6061e4a8ae24ed44a4416e1a7e3b13319cf7
bo.meng <bo.meng@jd.com>
2018-11-16 02:41:28 +0800

    Update household_demographics.py

--- tpcdsDataGenerator/dataParser/household_demographics.py
+++ tpcdsDataGenerator/dataParser/household_demographics.py
@@ -2,11 +2,12 @@ from pyspark.sql import Row
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import HiveContext
+import os
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 spark = HiveContext(sc)
 
-textDataRDD = sc.textFile("hdfs://master:9000/hibench/data/tables/household_demographics.dat")
+textDataRDD = sc.textFile(os.environ["DATA_HDFS"] + "household_demographics.dat")
 textDataDF = textDataRDD.map(lambda x: x.split("|")).map(lambda x: Row(hd_demo_sk = x[0],hd_income_band_sk = x[1],hd_buy_potential = x[2],hd_dep_count = x[3],hd_vehicle_count = x[4])).toDF()
 textDataDF.write.saveAsTable("tpcds.household_demographics", format="parquet", mode="overwrite")
