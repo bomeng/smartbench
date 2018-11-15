6cafc54ab137b8f607b667539b8be825756d0a9c
bo.meng <bo.meng@jd.com>
2018-11-16 02:41:50 +0800

    Update income_band.py

--- tpcdsDataGenerator/dataParser/income_band.py
+++ tpcdsDataGenerator/dataParser/income_band.py
@@ -2,11 +2,12 @@ from pyspark.sql import Row
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import HiveContext
+import os
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 spark = HiveContext(sc)
 
-textDataRDD = sc.textFile("hdfs://master:9000/hibench/data/tables/income_band.dat")
+textDataRDD = sc.textFile(os.environ["DATA_HDFS"] + "income_band.dat")
 textDataDF = textDataRDD.map(lambda x: x.split("|")).map(lambda x: Row(ib_income_band_sk = x[0],ib_lower_bound = x[1],ib_upper_bound = x[2])).toDF()
 textDataDF.write.saveAsTable("tpcds.income_band", format="parquet", mode="overwrite")
