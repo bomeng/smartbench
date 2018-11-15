008a33e79deaae6a206aa8a86d5a27853880d324
bo.meng <bo.meng@jd.com>
2018-11-16 02:48:59 +0800

    Update reason.py

--- tpcdsDataGenerator/dataParser/reason.py
+++ tpcdsDataGenerator/dataParser/reason.py
@@ -2,11 +2,12 @@ from pyspark.sql import Row
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import HiveContext
+import os
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 spark = HiveContext(sc)
 
-textDataRDD = sc.textFile("hdfs://master:9000/hibench/data/tables/reason.dat")
+textDataRDD = sc.textFile(os.environ["DATA_HDFS"] + "reason.dat")
 textDataDF = textDataRDD.map(lambda x: x.split("|")).map(lambda x: Row(r_reason_sk = x[0],r_reason_id = x[1],r_reason_desc = x[2])).toDF()
 textDataDF.write.saveAsTable("tpcds.reason", format="parquet", mode="overwrite")
